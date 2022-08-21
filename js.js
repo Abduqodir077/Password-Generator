f953b8f9ac206e141d4cdc1f19b595afce06bee9
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-08-21 21:50:15 +0500

    project end

--- js.js
+++ js.js
@@ -1,12 +1,12 @@
-let password = document.getElementById("password");
+var password = document.getElementById("password");
 
 function getPassword(){
-    let chars = "03S12#3gWEFyf1@212%vg32FDShgv3@2h&WERGWR23chg3*23D23!";
-    let passwordLength = 8;
-    let password = "";
+    var chars = "03S12#3gWEFyf1@212%vg32FDShgv3@2h&WERGWR23chg3*23D23!";
+    var passwordLength = 8;
+    var password = "";
 
-    for (let i = 0; i <= passwordLength; i++){
-        let randomNumber = Math.floor(Math.random() * chars.length);
+    for (var i = 0; i <= passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber +1);
     }
 
@@ -17,7 +17,7 @@ function getPassword(){
     // copy button
 
     function copyPassword(){
-        let copyText = document.getElementById("password");
+        var copyText = document.getElementById("password");
         copyText.select();
         copyText.setSelectionRange(0,999);
         document.execCommand("copy");
